cb18d6edafd8510a5b4ce5fcc2a29d7005e279fc
Tiiawss <tiia.nylen@gmail.com>
2020-11-06 20:54:52 +0200

    jtn

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -12,6 +12,37 @@ public class MaksukorttiTest {
     public void setUp() {
         kortti = new Maksukortti(10);
     }
+    
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(25);
+        assertEquals("saldo: 0.35", kortti.toString());
+    }
+     
+    @Test
+    public void kortiltaVoiOttaaRahaa() {
+        kortti.otaRahaa(25);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void korttiEiMeneMiinukselle() {
+        kortti.otaRahaa(45);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void rahatRiittavatOnTrue() {
+        assertTrue(kortti.otaRahaa(5)==true);      
+    }
+    public void falseJosRahatEiRiita() {
+        assertTrue(kortti.otaRahaa(40)==false);      
+    }
+    
+    
 
     @Test
     public void luotuKorttiOlemassa() {
