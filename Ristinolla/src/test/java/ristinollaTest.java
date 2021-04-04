8ee36ebde8712ba6a2e3ba7d71c010ddd02c050f
Tiiawss <tiia.nylen@gmail.com>
2021-04-04 16:39:44 +0300

    t

---
+++ Ristinolla/src/test/java/ristinollaTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nytiia
+ */
+public class ristinollaTest {
+    Ristinolla ristinolla;
+    
+    public ristinollaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        ristinolla= new Ristinolla();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    
+    @Test
+     public void listat() {
+         ristinolla.sijoitao(1, 1);
+         
+         assertEquals(ristinolla.rivi1.get(0),ristinolla.rivi1.get(0));
+         
+     
+}
+
+    @Test
+     public void hello() {}
+}
